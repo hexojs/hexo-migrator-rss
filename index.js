8734b8f926d5b83d0e0cd14f241f0c00a659db66
Tommy Chen <tommy351@gmail.com>
2014-04-27 13:28:13 +0800

    Refactor RSS migrator

--- index.js
+++ index.js
@@ -1,60 +1,69 @@
-var extend = hexo.extend,
-  util = hexo.util,
-  file = util.file,
-  feedparser = require('ortoo-feedparser'),
-  moment = require('moment'),
+var FeedParser = require('feedparser'),
   async = require('async'),
-  _ = require('underscore'),
-  tomd = require('to-markdown').toMarkdown;
+  tomd = require('to-markdown').toMarkdown,
+  request = require('request'),
+  fs = require('fs');
 
-extend.migrator.register('rss', function(args){
-  var source = args._.shift(),
-    target = hexo.source_dir + '_posts/';
+hexo.extend.migrator.register('rss', function(args, callback){
+  var source = args._.shift();
 
   if (!source){
-    console.log('\nUsage: hexo migrate rss <source>\n\nMore info: http://zespia.tw/hexo/docs/migration.html\n');
-  } else {
-    async.waterfall([
-      function(next){
-        console.log('Fetching %s.', source);
+    var help = [
+      'Usage: hexo migrate rss <source>',
+      '',
+      'For more help, you can check the docs: http://hexo.io/docs/migration.html'
+    ];
 
-        // URL regular expression from: http://blog.mattheworiordan.com/post/13174566389/url-regular-expression-for-links-with-or-without-the
-        if (source.match(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[.\!\/\\w]*))?)/)){
-          feedparser.parseUrl(source, next);
-        } else {
-          feedparser.parseFile(source, next);
-        }
-      },
-      function(meta, posts, next){
-        console.log('Analyzing.');
-
-        async.forEach(posts, function(item, next){
-          var postLink = (item.origlink ? item.origlink : item.link).split('/').reverse()[0];
-          if (!postLink) postLink = item.title.toLowerCase().split(' ').join('-');
-
-          if (item.categories){
-            if (_.isArray(item.categories)){
-              var tags = '\n- ' + item.categories.join('\n- ');
-            } else {
-              var tags = item.categories.toString();
-            }
-          }
-
-          var content = [
-            'title: ' + item.title,
-            'date: ' + moment(item.pubdate).format('YYYY-MM-DD HH:mm:ss'),
-            'tags: ' + (tags ? tags : ''),
-            '---',
-          ];
-
-          file.write(target + postLink + '.md', content.join('\n') + '\n\n' + tomd(item.description), next);
-        }, function(){
-          next(null, posts.length);
-        });
-      }
-    ], function(err, length){
-      if (err) throw err;
-      console.log('%d posts migrated.', length);
-    });
+    console.log(help.join('\n'));
+    return callback();
   }
+
+  var log = hexo.log,
+    post = hexo.post,
+    stream;
+
+  // URL regular expression from: http://blog.mattheworiordan.com/post/13174566389/url-regular-expression-for-links-with-or-without-the
+  if (source.match(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[.\!\/\\w]*))?)/)){
+    stream = request(source);
+  } else {
+    stream = fs.createReadStream(source);
+  }
+
+  log.i('Analyzing %s...', source);
+
+  var feedparser = new FeedParser(),
+    posts = [];
+
+  stream.pipe(feedparser)
+    .on('error', callback);
+
+  feedparser.on('error', callback);
+
+  feedparser.on('readable', function(){
+     var stream = this,
+      meta = this.meta,
+      item;
+
+    while (item = stream.read()){
+      posts.push({
+        title: item.title,
+        date: item.date,
+        tags: item.tags,
+        content: tomd(item.description)
+      });
+
+      log.i('Post found: %s', item.title);
+    }
+  });
+
+  stream.on('end', function(){
+    async.each(posts, function(item, next){
+      post.create(item, next);
+    }, function(err){
+      if (err) return callback(err);
+
+      log.i('%d posts migrated.', posts.length);
+      callback();
+    });
+  });
 });
