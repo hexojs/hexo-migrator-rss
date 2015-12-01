714347ce74105af40f1a2b5e0ee16b59e0a6d3ea
dsolovay <dsolovay@gmail.com>
2015-12-01 01:08:52 -0500

    Refactor tests to use aysnc pattern
    Use mocha async pattern, in which a callback function
    containing the assertions is passed to the method under
    test.  The callback function executes the assertions
    and then calls done() to indicate that it has completed.
    See: https://mochajs.org/#asynchronous-code

--- test/fakehexoFactory.js
+++ test/fakehexoFactory.js
@@ -5,26 +5,27 @@ exports.create = function () {
 	{
 		registeredType: null,
 		receivedPosts: [],
-		callback: null,
+		registeredFunction: null,
 		calledType: null
 	};
 	mock.extend = {};
 	mock.extend.migrator = {};
 	mock.extend.migrator.register = function (type, f) {
 		mock.setValues.registeredType = type;
-		mock.setValues.callback = f;
+		mock.setValues.registeredFunction = f;
 	};
-	mock.call = function (type, args) {
+	mock.call = function (type, args, callback) {
 		mock.setValues.calledType = args._.shift();
-		mock.setValues.callback(args, function () { });		
+		mock.setValues.registeredFunction(args, callback);		
 	};
 	mock.log = {
 		i: function () { },
 		w: function () { }
 	};
 	mock.post = {
-		create: function (newPost) {
+		create: function (newPost, next) {
 			mock.setValues.receivedPosts.push(newPost);
+			next();
 		}
 	};
 	return mock;
--- test/index.js
+++ test/index.js
@@ -5,44 +5,52 @@ var should = require('chai').should(),
 	fakehexoFactory = require('./fakehexoFactory.js'),
     fakeHexo;
 
-describe("migrator rss", function () {
+describe("migrator", function () {
+	
+	this.timeout(10000);
+	
 	beforeEach(function () {
 		fakeHexo = fakehexoFactory.create();
 		migrator.registerMigrator(fakeHexo);
 	});
 
-	it("registers \"rss\" to hexo", function () {
+	it("registers rss", function () {
 		fakeHexo.setValues.registeredType.should.equal("rss");
 	});
 
     context("called with \"rss\"", function () {
-		it("passes parameter through", function () {
-			fakeHexo.call("migrate", { _: ["rss"] });
-			fakeHexo.setValues.calledType.should.equal("rss");
-		})
-
-	});
-
-	context("passed source without alias flag", function () {
-		it("creates posts without alias", function (done) {
-			fakeHexo.call("migrate", { _: ["rss", "https://github.com/danmactough/node-feedparser/raw/master/test/feeds/rss2sample.xml"] });
-			setTimeout(function () {
-				fakeHexo.setValues.receivedPosts.length.should.be.gt(0);
-				should.not.exist(fakeHexo.setValues.receivedPosts[0].alias);
+		it("passes parameter through", function (done) {
+			fakeHexo.call("migrate", { _: ["rss"] }, function (err) {
+				if (err) throw err;
+				fakeHexo.setValues.calledType.should.equal("rss");
 				done();
-			}, 500);
+			});
 		});
 	});
 
-	context("passed source with alias flag", function () {
-		it("creates posts with alias", function (done) {
-			fakeHexo.call("migrate", { _: ["rss", "https://github.com/danmactough/node-feedparser/raw/master/test/feeds/rss2sample.xml"], alias: true });
-			setTimeout(function () {
-				fakeHexo.setValues.receivedPosts.length.should.be.gt(0);
-				should.exist(fakeHexo.setValues.receivedPosts[0].alias, "alias missing");
-				done();
-			}, 500);
+	context("alias flag not passed", function () {
+
+		it("creates posts without alias field", function (done) {
+			fakeHexo.call("migrate", { _: ["rss", "https://github.com/danmactough/node-feedparser/raw/master/test/feeds/rss2sample.xml"] },
+				function (err) {
+					if (err) throw err;
+					fakeHexo.setValues.receivedPosts.length.should.be.gt(0);
+					should.not.exist(fakeHexo.setValues.receivedPosts[0].alias);
+					done();
+				});
 		});
-	});	
+	});
+
+	context("alias flag passed", function () {
+		it("creates posts with alias field", function (done) {
+			fakeHexo.call("migrate", { _: ["rss", "https://github.com/danmactough/node-feedparser/raw/master/test/feeds/rss2sample.xml"], alias: true },
+				function (err) {
+					if (err) throw err;
+					fakeHexo.setValues.receivedPosts.length.should.be.gt(0);
+					should.exist(fakeHexo.setValues.receivedPosts[0].alias, "alias missing");
+					done();
+				});
+		});
+	});
 });
 
